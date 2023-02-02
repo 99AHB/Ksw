3d049093d168e1a9b533d6b1fc223d31acc58af8
99AHB <122578987+99AHB@users.noreply.github.com>
2023-02-02 18:21:36 +0900

    연습문제10-6

--- K-Empowerment-Software-Bootcamp-master/연습문제10장.py
+++ K-Empowerment-Software-Bootcamp-master/연습문제10장.py
@@ -6,6 +6,8 @@
 # A=Thing()
 # print(A) #A 객체 추가
 # #두 출력값은 다르다
+import asyncio
+
 
 # #Ex02
 # class thing2():
@@ -20,19 +22,29 @@
 # print(A.letters)
 
 # EX04
-class Element():
-     def __init__(sc,name,symbol,number):
-         sc.name=name
-         sc.symbol=symbol
-         sc.number=number
-B = Element('Hydrogen','H',1)
-print(B.name,B.symbol,B.number)
+# class Element():
+#      def __init__(sc,name,symbol,number):
+#          sc.name=name
+#          sc.symbol=symbol
+#          sc.number=number
+# B = Element('Hydrogen','H',1)
+# print(B.name,B.symbol,B.number)
+#
+# # EX05
+# el_dict={'name' : 'Hydrogen', 'symbol' : 'H', 'number': 1 }
+# hydrogen=Element(el_dict['name'],el_dict['symbol'],el_dict['number'])
+# print(hydrogen.name)
 
-# EX05
-el_dict={'name' : 'Hydrogen', 'symbol' : 'H', 'number': 1 }
-hydrogen=Element(el_dict['name'],el_dict['symbol'],el_dict['number'])
-print(hydrogen.name)
-
+# EX06
+# class Element():
+#     def __init__(A,name,symbol,number):
+#         A.name=name
+#         A.symbol=symbol
+#         A.number=number
+#     def dump(B):
+#         print(B.name,B.symbol,B.number)
+# hydregonn=Element(**ed_dick)
+# print(hydregonn.dump())
 
 
 
