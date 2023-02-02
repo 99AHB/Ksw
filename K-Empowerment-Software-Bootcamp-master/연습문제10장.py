5072f2cfcac3f9698b29fad79500ca69a1b9673c
99AHB <122578987+99AHB@users.noreply.github.com>
2023-02-02 17:16:46 +0900

    연습문제1~4

---
+++ K-Empowerment-Software-Bootcamp-master/연습문제10장.py
@@ -0,0 +1,40 @@
+# # Ex01
+# class Thing():
+#     pass
+# print(Thing) #아무것도 없는 class
+#
+# A=Thing()
+# print(A) #A 객체 추가
+# #두 출력값은 다르다
+
+# #Ex02
+# class thing2():
+#     letters = 'abc'
+# print(thing2.letters)
+
+# # EX03
+# class Thing3():
+#     def __init__(self):
+#        self.letters='xyz'
+# A=Thing3()
+# print(A.letters)
+
+# EX04
+# class Element():
+#     def __init__(sc,name,symbol,number):
+#         sc.name=name
+#         sc.symbol=symbol
+#         sc.number=number
+# B = Element('Hydrogen','H',1)
+# print(B.name,B.symbol,B.number)
+
+
+
+
+
+
+
+
+
+
+
