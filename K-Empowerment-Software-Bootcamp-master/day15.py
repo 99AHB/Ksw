6e488ad22450fbadd00ca7444d39727931ddb63a
99AHB <122578987+99AHB@users.noreply.github.com>
2023-02-03 14:24:40 +0900

    day15 linear list 07 (수정후)

--- K-Empowerment-Software-Bootcamp-master/day15.py
+++ K-Empowerment-Software-Bootcamp-master/day15.py
@@ -14,31 +14,29 @@ def print_poly(px, tx):
             poly_str = poly_str + "+"
 
         poly_str = poly_str + f'{coef}x^{term} '
-        term = term - 1
 
     return poly_str
 
 
-def calc_poly(x_val, px):
+def calc_poly(x_val, px, tx):
     """
     다항식의 산술연산을 하는 함수
     :param x_val: x값 integer
-    :param p_x: 계수를 원소로 가지고 있는 list
+    :param px: 계수를 원소로 가지고 있는 list
+    :param tx: 차수를 원소로 가지고 있는 list
     :return: 다항식 계산 결과 값 integer
     """
     return_val = 0
 
     for i in range(len(px)):
-        term= tx[1]
+        term = tx[i]
         coef = px[i]
         return_val = return_val + coef * x_val ** term
-        term = term - 1
 
     return return_val
 
 
-# px = [3, -4, 0, 6]
-px = [7, -4, 5]
+px = [3, -4, 5]
 tx = [300, 20, 0]
 
 if __name__ == "__main__":
