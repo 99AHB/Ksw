29a058e8a1a834e0302ea333e05b76a670279988
99AHB <122578987+99AHB@users.noreply.github.com>
2023-02-03 10:48:02 +0900

    day15

---
+++ K-Empowerment-Software-Bootcamp-master/day15.py
@@ -0,0 +1,14 @@
+pokemons = list()
+
+def add_data(pokemon):
+    pokemons.append(None)
+    # pokemons[len(pokemons) - 1] = pokemon
+    pokemons[- 1] = pokemon
+
+add_data('피카츄')
+add_data('라이츄')
+add_data('파이리')
+add_data('꼬부기')
+add_data('이상해')
+
+print(pokemons)
