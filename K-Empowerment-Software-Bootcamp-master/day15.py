241561b29c0ce804f6ca9cebf8a0c60666c937b3
99AHB <122578987+99AHB@users.noreply.github.com>
2023-02-03 13:23:05 +0900

    day15 linear list 05 (수정전)

--- K-Empowerment-Software-Bootcamp-master/day15.py
+++ K-Empowerment-Software-Bootcamp-master/day15.py
@@ -1,5 +1,5 @@
 def insert_data(idx, pokemon):
-    if idx < 0 or idx > len(pokemons):
+    if idx < 0 or idx > len():
         print("Out of range!")
         return
 
@@ -20,12 +20,27 @@ def delete_data(idx):
     len_pokemons = len(pokemons)
     pokemons[idx] = None
 
+    # self 3-1
+    # for i in range(len_pokemons - idx):
+    #     pokemons.pop()
     for i in range(idx + 1, len_pokemons):
         pokemons[i - 1] = pokemons[i]
         pokemons[i] = None
 
     pokemons.pop()
 
+
+
+def add_data(pokemon):
+    """
+    선형 리스트의 맨 뒤에 원소 삽입
+    :param pokemon:str
+    :return:
+    """
+    pokemons.append(None)
+    pokemons[len(pokemons)-1] = pokemons
+
+pokemons=[]
 
 if __name__ == "__main__":
     pokemons = ["피카츄", "라이츄", "꼬부기", "파이리", "이상해"]
@@ -36,3 +51,28 @@ if __name__ == "__main__":
     # #insert_data(6, '거북왕')
     delete_data(3)
     print(pokemons)
+    add_data('터검니')
+    print(pokemons)
+    while True:
+        select = int(input("선택하세요(1: 추가, 2: 삽입, 3: 삭제, 4: 종료)--> "))
+
+        if (select == 1):
+            data = input("추가할 데이터--> ")
+            add_data(data)
+            print(pokemons)
+        elif (select == 2):
+            pos = int(input("삽입할 위치--> "))
+            data = input("추가할 데이터--> ")
+            insert_data(pos, data)
+            print(pokemons)
+        elif (select == 3):
+            pos = int(input("삭제할 위치--> "))
+            delete_data(pos)
+            print(pokemons)
+        elif (select == 4):
+            print(pokemons)
+            # exit()
+            break
+        else:
+            print("menu에서 고르세요.")
+            continue
