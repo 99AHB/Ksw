906da34c36c94fb15f5b0db3c6982d4e6df60143
99AHB <122578987+99AHB@users.noreply.github.com>
2023-02-03 18:15:05 +0900

    20230203ex

--- K-Empowerment-Software-Bootcamp-master/연습문제용.py
+++ K-Empowerment-Software-Bootcamp-master/연습문제용.py
@@ -1,58 +1,38 @@
-# ex
-def find_and_insert_data(pokemons,h_count) :
+
+def find_and_insert_data(friend, k_count):
     findPos = -1
-    for i in range(len(hp)):
-        pair = hp[i]
-        if h_count >= pair[1]:
+    for i in range(len(katok)):
+        pair = katok[i]
+        if k_count >= pair[1]:
             findPos = i
             break
-    if findPos == -1 :
-        findPos = len(hp)
+    if findPos == -1:
+        findPos = len(katok)
 
-    insert_data(findPos, (pokemons, h_count))
+    insert_data(findPos, (friend, k_count))
 
 
-def insert_data(position,pokemons):
-    if position < 0 or position > len(hp):
-        print("입력값을 다시 입력해주세요")
+def insert_data(position, friend):
+    if position < 0 or position > len(katok):
+        print("데이터를 삽입할 범위를 벗어났습니다.")
         return
 
-    hp.append(None)
-    hLen = len(hp)
 
-    for i in range(hLen -1, position, -1):
-        hp[i] = hp[i-1]
-        hp[i-1] = None
+    katok.append(None)
+    kLen = len(katok)
 
-    hp[position] = pokemons
+    for i in range(kLen - 1, position, -1):
+        katok[i] = katok[i -1]
+        katok[i -1] = None
 
-hp = {"라이츄":200 , "피카츄":150, "파이리":100, "꼬부기":70, "치코리타":1}
-sort_hp =sorted(hp.items(),key=lambda x:x[1], reverse=True)
-# print(sort_hp)
+    katok[position] = friend
+
+katok = ['다현', 200], ['정연', 150],['쯔위', 90], ['사나', 30], ['지효', 15]
 
 if __name__ == "__main__":
 
-    while True :
-        data = input("추가할 pokemons--> ")
-        count = int(input("포켓몬의 체력 입력-->"))
-        find_and_insert_data(data,count)
-print(sort_hp)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# 1. 선형리스트
-# 2. 1
-# 3. 4->2->3->1
-# 4.
+    while True:
+        data = input("추가할 친구--> ")
+        count = int(input("카톡 횟수--> "))
+        find_and_insert_data(data, count)
+        print(katok)
