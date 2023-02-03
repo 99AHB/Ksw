a68f9e1ad7f3af04fcb34ebb33d2bd5987e54157
99AHB <122578987+99AHB@users.noreply.github.com>
2023-02-03 11:30:45 +0900

    day15 linear list 03 (수정필요)

--- K-Empowerment-Software-Bootcamp-master/day15.py
+++ K-Empowerment-Software-Bootcamp-master/day15.py
@@ -11,12 +11,38 @@ def insert_data(idx, pokemon):
 
     pokemons[idx] = pokemon
 
+def delete_date(idx):
+    pokemons = ['피카츄", "라이츄", "꼬부기", "파이리", "이상해']
+
+    def delete_data(idx):
+
+        if idx < 0 or idx > len(pokemons):
+            print("Out of rangs.")
+            return
+
+        len_pokemons = len(pokemons)
+        pokemons[idx] = None
+
+        for i in range(idx + 1, len_pokemons):
+            pokemons[i - 1] = pokemons[i]
+            pokemons[i] = None
+
+        del (pokemons[len_pokemons - 1])
+
+
+    delete_data(1)
+    print(pokemons)
+    delete_data(3)
+    print(pokemons)
+
 
 if __name__ == "__main__":
     pokemons = ["피카츄", "라이츄", "꼬부기", "파이리", "이상해"]
     print(pokemons)
-    insert_data(3, '어니부기')
+    #insert_data(1)
+    delete_date(1)
     print(pokemons)
-    insert_data(6, '거북왕')
+    #insert_data(3)
+    delete_date(3)
     print(pokemons)
 
