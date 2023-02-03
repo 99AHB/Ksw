0675785435c3d4ab3bfab728e87e9bdf83ba9f14
99AHB <122578987+99AHB@users.noreply.github.com>
2023-02-03 14:46:54 +0900

    day15 linear list 예제1 (다시해보기)

--- K-Empowerment-Software-Bootcamp-master/day15.py
+++ K-Empowerment-Software-Bootcamp-master/day15.py
@@ -1,46 +1,38 @@
-def print_poly(px, tx):
-    """
-    다항식을 포맷에 맞게 출력하는 함수
-    :param px: 계수를 원소로 가지고 있는 list
-    :param tx: 차수를 원소로 가지고 있는 list
-    :return: 다항식 문자열
-    """
-    poly_str = "P(x) = "
+def find_and_insert_data(friend, K_count):
+    findPos = -1
+    for i in range(len(kakao)):
+        pair = kakao[i]
+        if K_count > pair[1]:  #pair는 숫자를 의미한다
+            findPos = i
+            break
+        if findPos == -1:
+            findPos = len(kakao)
 
-    for i in range(len(px)):
-        term = tx[i]
-        coef = px[i]
-        if coef >= 0:
-            poly_str = poly_str + "+"
-
-        poly_str = poly_str + f'{coef}x^{term} '
-
-    return poly_str
+        insert_data(findPos, (friend, K_count))
 
 
-def calc_poly(x_val, px, tx):
-    """
-    다항식의 산술연산을 하는 함수
-    :param x_val: x값 integer
-    :param px: 계수를 원소로 가지고 있는 list
-    :param tx: 차수를 원소로 가지고 있는 list
-    :return: 다항식 계산 결과 값 integer
-    """
-    return_val = 0
+def insert_data(position, friend):
+    if position < 0 or position > len(kakao):
+        print("데이터 삽입 범위를 초과하였습니다.")
+        return
 
-    for i in range(len(px)):
-        term = tx[i]
-        coef = px[i]
-        return_val = return_val + coef * x_val ** term
+    kakao.append(None)
+    kLen = len(kakao)
 
-    return return_val
+    for i in range(kLen -1, position -1):
+        kakao[i] = kakao[i-1]
+        kakao[i-1] = None
+
+    kakao[position] = friend
 
 
-px = [3, -4, 5]
-tx = [300, 20, 0]
+kakao = [('다현', 200),('정현',150),('쯔위', 90),('사나', 30),('지효',150)]
+
 
 if __name__ == "__main__":
-    print(print_poly(px, tx))
 
-    x_value = int(input("X 값 : "))
-    print(calc_poly(x_value, px, tx))
+    while True:
+        data = input("추가할 친구 --> ")
+        count = int(input("카톡 횟수__> "))
+        find_and_insert_data(data, count)
+        print(kakao)
