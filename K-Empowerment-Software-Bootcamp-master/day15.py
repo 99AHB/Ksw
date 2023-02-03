49a0aa817d0cb7a8c85c98a139c433d2824e58b9
99AHB <122578987+99AHB@users.noreply.github.com>
2023-02-03 11:17:25 +0900

    day15 linear list 02

--- K-Empowerment-Software-Bootcamp-master/day15.py
+++ K-Empowerment-Software-Bootcamp-master/day15.py
@@ -1,14 +1,22 @@
-pokemons = list()
+def insert_data(idx, pokemon):
+    if idx < 0 or idx > len(pokemons):
+        print("Out of range!")
+        return
 
-def add_data(pokemon):
     pokemons.append(None)
-    # pokemons[len(pokemons) - 1] = pokemon
-    pokemons[- 1] = pokemon
 
-add_data('피카츄')
-add_data('라이츄')
-add_data('파이리')
-add_data('꼬부기')
-add_data('이상해')
+    for i in range(len(pokemons) - 1, idx, -1):
+        pokemons[i] = pokemons[i - 1]
+        pokemons[i - 1] = None
 
-print(pokemons)
+    pokemons[idx] = pokemon
+
+
+if __name__ == "__main__":
+    pokemons = ["피카츄", "라이츄", "꼬부기", "파이리", "이상해"]
+    print(pokemons)
+    insert_data(3, '어니부기')
+    print(pokemons)
+    insert_data(6, '거북왕')
+    print(pokemons)
+
