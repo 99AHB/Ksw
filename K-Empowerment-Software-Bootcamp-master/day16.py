5bd28d44d252283f0509e480556e04737fba0207
99AHB <122578987+99AHB@users.noreply.github.com>
2023-02-06 14:59:54 +0900

    day16 stack 01

--- K-Empowerment-Software-Bootcamp-master/day16.py
+++ K-Empowerment-Software-Bootcamp-master/day16.py
@@ -2,7 +2,7 @@ stack = [None for _ in range(5)]
 top = -1
 
 top = top + 1
-stack[top] = "커피"
+stack[top] = "아이스아메리카노"
 top = top + 1
 stack[top] = "녹차"
 top = top + 1
@@ -11,6 +11,9 @@ top = top + 1
 stack[top] = "오렌지주스"
 top = top + 1
 stack[top] = "바닐라라떼"
+# stack overflow
+# top = top + 1
+# stack[top] = "카라멜마끼아또"
 
 for i in range(len(stack)-1, -1, -1):
     print(stack[i])
@@ -32,8 +35,3 @@ print(data)
 
 for i in range(len(stack)-1, -1, -1):
     print(stack[i])
-
-data = stack[top]
-stack[top] = None
-top = top - 1
-print(data)
