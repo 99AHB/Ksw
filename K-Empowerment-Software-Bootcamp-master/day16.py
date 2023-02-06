696e183754f781fff37b45021cbbe7e697aa7db0
99AHB <122578987+99AHB@users.noreply.github.com>
2023-02-06 15:34:59 +0900

    day16 stack 02 (me)

--- K-Empowerment-Software-Bootcamp-master/day16.py
+++ K-Empowerment-Software-Bootcamp-master/day16.py
@@ -1,37 +1,68 @@
-stack = [None for _ in range(5)]
+def is_stack_full():
+    global SIZE, stack, top
+    if top >= SIZE-1:
+        return True
+    return False
+
+
+def is_stack_empty():
+    global SIZE, stack, top
+    if top >= SIZE - 1:
+        return True
+    return False
+
+
+def push():
+    global SIZE, stack, top
+    if is_stack_full():
+        print("Stack is FULL!")
+        return
+    top = top + 1
+    stack[top] = data
+
+
+def pop():
+    global SIZE, stack, top
+    if is_stack_empty():
+        print("Stack ifs EMPTY~")
+        return
+    temp = stack[top]
+    stack[top] = None
+    top = top - 1
+    return temp
+
+def peek():
+    global SIZE, stack, top
+    if is_stack_empty():
+        print("Stack ifs EMPTY~")
+        return None
+    return stack[top]
+
+
+SIZE = int(input("Stack Size : "))
+stack = [None for _ in range(SIZE)]
 top = -1
 
-top = top + 1
-stack[top] = "아이스아메리카노"
-top = top + 1
-stack[top] = "녹차"
-top = top + 1
-stack[top] = "꿀물"
-top = top + 1
-stack[top] = "오렌지주스"
-top = top + 1
-stack[top] = "바닐라라떼"
-# stack overflow
-# top = top + 1
-# stack[top] = "카라멜마끼아또"
 
-for i in range(len(stack)-1, -1, -1):
-    print(stack[i])
+if __name__ == "__main__":
+        select = input("삽입(I)/추출(E)/확인(V)/종료(X) 중 하나를 선택 ==> ")
 
-data = stack[top]
-stack[top] = None
-top = top - 1
-print(data)
+        while (select != 'X' and select != 'x'):
+            if select == 'I' or select == 'i':
+                data = input("입력할 데이터 ==> ")
+                push()
+                print("스택 상태 : ", stack)
+            elif select == 'E' or select == 'e':
+                data = pop()
+                print("추출된 데이터 ==> ", data)
+                print("스택 상태 : ", stack)
+            elif select == 'V' or select == 'v':
+                data = peek()
+                print("확인된 데이터 ==> ", data)
+                print("스택 상태 : ", stack)
+            else:
+                print("입력이 잘못됨")
 
-data = stack[top]
-stack[top] = None
-top = top - 1
-print(data)
+            select = input("삽입(I)/추출(E)/확인(V)/종료(X) 중 하나를 선택 ==> ")
 
-data = stack[top]
-stack[top] = None
-top = top - 1
-print(data)
-
-for i in range(len(stack)-1, -1, -1):
-    print(stack[i])
+        print("프로그램 종료!")
