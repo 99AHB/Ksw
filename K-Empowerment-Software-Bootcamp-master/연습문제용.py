3089d66d8cfb63a3e6342847526b2d7c4b7dbe25
99AHB <122578987+99AHB@users.noreply.github.com>
2023-02-06 18:07:24 +0900

    5-2 EX

--- K-Empowerment-Software-Bootcamp-master/연습문제용.py
+++ K-Empowerment-Software-Bootcamp-master/연습문제용.py
@@ -1,79 +1,50 @@
-# # 5-1 편의점 관리하기
-# import random
-# import math
-#
-# class Node():
-#     def __init__(self):
-#         self.data = None
-#         self.link = None
-#
-# def printStores(start):
-#     current = start
-#     if current == None:
-#         return
-#
-#     while current.link != head :
-#         current = current.link
-#         x, y = current.data[1:]
-#         print(current.data[0], '편의점, 거리:', math.sqrt(x*x + y*y))
-#     print()
-#
-# def makeStoreList(store) :
-#     global memory,head,current,pre
-#
-#     node = Node()
-#     node.data = store
-#     memory.append(node)
-#
-#     if head == None:
-#         head = Node
-#         node.link = head
-#         return
-#
-#     nodeX, nodeY = node.data[1:]
-#     nodeDist = math.sqrt(nodeX*nodeX + nodeY*nodeY)
-#     headX, headY = head.data[1:]
-#     headDist = math.sqrt(headX*headX + headY*headY)
-#
-#     if headDist > nodeDist:
-#         node.link = head
-#         last = head
-#         while last.link != head:
-#             last = last.link
-#         last.link = node
-#         head = node
-#         return
-#
-#     current = head
-#     while current.link != head:
-#         pre = current
-#         current = current.link
-#         currentX,currentY = current.data[1:]
-#         currentDist = math.sqrt(currentX*currentX + currentY*currentY)
-#         if currentDist > nodeDist :
-#             pre.link = node
-#             node.link = current
-#             return
-#
-#     current.link = node
-#     node.link = head
-#
-# memory = []
-# head, current, pre = None, None, None
-#
-# if __name__ == "__main__":
-#
-#     storeArray = []
-#     storeName = 'A'
-#     for _ in range(10):
-#         store = (storeName, random.randint(1,100), random.randint(1,100))
-#         storeArray.append(store)
-#         storeName = chr(ord(storeName) + 1)
-#
-#     for store in storeArray:
-#         makeStoreList(store)
-#
-#     printStores(head)
+# 5-2 이중 연결 리스트 구현
+class NodeA():
+    def __init__(self):
+        self.Alink=None
+        self.data=None
+        self.Zlink=None
+
+def printNodes(start):
+    current = start
+    if current.Alink ==None:
+        return
+    print("정방향-->", end=' ')
+    print(current.data,end = ' ')
+    while current.Alink !=None:
+        current = current.Alink
+        print(current.data, end=' ')
+    print()
+    print("역방항-->", end=' ')
+    print(current.data,end=' ')
+    while current.Zlink !=None:
+        current = current.Zlink
+        print(current.data, end=' ')
+
+
+    memory = []
+    head, current, pre = None, None, None
+    dataName = ["다현", "정연", "쯔위", "사나", "지효"]
+
+if __name__ == "__name__":
+
+    node = NodeA()
+    node.data = dataArray[0]
+    head= node
+    memory.append(node)
+
+    for data in dataArray[1:]:
+        pre = node
+        node = NodeA()
+        node.data = data
+        pre.Alink = node
+        pre.Zlink = pre
+        memory.append(node)
+
+    printNodes(head)
+
+
+
 
 
 
