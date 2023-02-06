f1a5c288c296163005d35a718e59aa2e937ec729
99AHB <122578987+99AHB@users.noreply.github.com>
2023-02-06 10:42:14 +0900

    day16 02

--- K-Empowerment-Software-Bootcamp-master/day16.py
+++ K-Empowerment-Software-Bootcamp-master/day16.py
@@ -1,7 +1,8 @@
 class Node:
-	def __init__(self):
-		self.data = data
-		self.link = None
+    def __init__(self, data):
+        self.data = data
+        self.link = None
+
 
 def print_nodes(start):
     current = start
@@ -9,40 +10,39 @@ def print_nodes(start):
         return
     print(current.data, end=' ')
     while current.link != None:
-        current = current.link  # 다음 노드로 이동
+        current = current.link
         print(current.data, end=' ')
     print()
 
-def insert(find_data, insert_data):
-    global memory, head, current, property
-    if head.data == find_data:  # 첫 번째 노드 삽입
-        node = Node()
-        node.data = insert_data
+
+def insert_nodes(find_data, insert_data):
+    global head, current, pre
+    if head.data == find_data:
+        node = Node(insert_data)
         node.link = head
         head = node
         return
 
     current = head
-    while current.link != None:  # 중간 노드 삽입
+    while current.link != None:
         pre = current
         current = current.link
         if current.data == find_data:
-            node = Node()
-            node.data = insert_data
+            node = Node(insert_data)
             node.link = current
             pre.link = node
             return
 
-    node = Node()  # 마지막 노드 삽입
-    node.data = insert_data
+    node = Node(insert_data)
     current.link = node
 
-def delete_nodes(delelt_data):
-    global memory, head, current, pre
 
-    if head.data == delelt_data:
+def delete_nodes(delete_data):
+    global head, current, pre
+
+    if head.data == delete_data:
+        print('첫 번째 노드 삭제 완료')
         current = head
-        print("첫번쨰 노드가 삭제됨")
         head = head.link
         del current
         return
@@ -51,44 +51,42 @@ def delete_nodes(delelt_data):
     while current.link != None:
         pre = current
         current = current.link
-        if current.data == delelt_data:
-            print("중간 노드가 삭제됨")
+        if current.data == delete_data:
+            print('중간 노드 삭제 완료')
             pre.link = current.link
             del current
             return
 
-    print("삭제할 노드를 찾기 못함")
+    print('삭제할 노드를 찾지 못함')
 
-    # 삭제할 데이터를 못찾는 경우 함수 종료
 
 def find_nodes(find_data):
-    global memory, head, current, pre
+    global head, current, pre
 
     current = head
     if current.data == find_data:
         return current
 
-    while current.link !=None:
+    while current.link != None:
         current = current.link
+        if current.data == find_data:
+            return current
+
+    return Node(None)
 
 
-
-memory = []
 head, current, pre = None, None, None
 data_array = ["피카츄", "라이츄", "꼬부기", "파이리", "이상해"]
 
+
 if __name__ == "__main__":
-    node = Node()
-    node.data = data_array[0]
+    node = Node(data_array[0])
     head = node
-    memory.append(node)
 
     for data in data_array[1:]:
         pre = node
-        node = Node()
-        node.data = data
+        node = Node(data)
         pre.link = node
-        memory.append(node)
 
     print_nodes(head)
     insert_nodes("피카츄", "잠만보")
@@ -96,11 +94,12 @@ if __name__ == "__main__":
     insert_nodes("파이리", "어니부기")
     print_nodes(head)
     insert_nodes("성윤모", "거북왕")
+    print_nodes(head)
     delete_nodes("잠만보")
     print_nodes(head)
     delete_nodes("어니부기")
     print_nodes(head)
     delete_nodes("강찬석")
     print_nodes(head)
-    print_nodes(find_nodes("파이리"))
-    print_nodes(find_nodes("박민석"))
+    print(find_nodes("파이리").data)
+    print(find_nodes("박민석").data)
