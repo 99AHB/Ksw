dd3cbb4b6cb561383fa89462cdc41732b3ac9e7b
99AHB <122578987+99AHB@users.noreply.github.com>
2023-02-06 17:18:06 +0900

    5-1ex

--- K-Empowerment-Software-Bootcamp-master/연습문제용.py
+++ K-Empowerment-Software-Bootcamp-master/연습문제용.py
@@ -1,38 +1,84 @@
+# # 5-1 편의점 관리하기
+# import random
+# import math
+#
+# class Node():
+#     def __init__(self):
+#         self.data = None
+#         self.link = None
+#
+# def printStores(start):
+#     current = start
+#     if current == None:
+#         return
+#
+#     while current.link != head :
+#         current = current.link
+#         x, y = current.data[1:]
+#         print(current.data[0], '편의점, 거리:', math.sqrt(x*x + y*y))
+#     print()
+#
+# def makeStoreList(store) :
+#     global memory,head,current,pre
+#
+#     node = Node()
+#     node.data = store
+#     memory.append(node)
+#
+#     if head == None:
+#         head = Node
+#         node.link = head
+#         return
+#
+#     nodeX, nodeY = node.data[1:]
+#     nodeDist = math.sqrt(nodeX*nodeX + nodeY*nodeY)
+#     headX, headY = head.data[1:]
+#     headDist = math.sqrt(headX*headX + headY*headY)
+#
+#     if headDist > nodeDist:
+#         node.link = head
+#         last = head
+#         while last.link != head:
+#             last = last.link
+#         last.link = node
+#         head = node
+#         return
+#
+#     current = head
+#     while current.link != head:
+#         pre = current
+#         current = current.link
+#         currentX,currentY = current.data[1:]
+#         currentDist = math.sqrt(currentX*currentX + currentY*currentY)
+#         if currentDist > nodeDist :
+#             pre.link = node
+#             node.link = current
+#             return
+#
+#     current.link = node
+#     node.link = head
+#
+# memory = []
+# head, current, pre = None, None, None
+#
+# if __name__ == "__main__":
+#
+#     storeArray = []
+#     storeName = 'A'
+#     for _ in range(10):
+#         store = (storeName, random.randint(1,100), random.randint(1,100))
+#         storeArray.append(store)
+#         storeName = chr(ord(storeName) + 1)
+#
+#     for store in storeArray:
+#         makeStoreList(store)
+#
+#     printStores(head)
 
-def find_and_insert_data(friend, k_count):
-    findPos = -1
-    for i in range(len(katok)):
-        pair = katok[i]
-        if k_count >= pair[1]:
-            findPos = i
-            break
-    if findPos == -1:
-        findPos = len(katok)
-
-    insert_data(findPos, (friend, k_count))
 
 
-def insert_data(position, friend):
-    if position < 0 or position > len(katok):
-        print("데이터를 삽입할 범위를 벗어났습니다.")
-        return
 
 
-    katok.append(None)
-    kLen = len(katok)
 
-    for i in range(kLen - 1, position, -1):
-        katok[i] = katok[i -1]
-        katok[i -1] = None
 
-    katok[position] = friend
 
-katok = ['다현', 200], ['정연', 150],['쯔위', 90], ['사나', 30], ['지효', 15]
-
-if __name__ == "__main__":
-
-    while True:
-        data = input("추가할 친구--> ")
-        count = int(input("카톡 횟수--> "))
-        find_and_insert_data(data, count)
-        print(katok)
