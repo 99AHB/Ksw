218a21b59efe671db861c82309e73e713cef93ce
99AHB <122578987+99AHB@users.noreply.github.com>
2023-02-09 10:25:07 +0900

    day19 recursion01 fibo ( 놓침 )

--- K-Empowerment-Software-Bootcamp-master/day19.py
+++ K-Empowerment-Software-Bootcamp-master/day19.py
@@ -1,13 +1,20 @@
-def fibo_recu(n) :
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else :
-        return fibo_recu(n-1) + fibo_recu(n-2)
+memo = list() # 전역 변수를 한 번 처리한 결과 값을 저장
+def fibo_memo(n) :
+    global memo
+    memo = [1, 1]
+    """
+    Memoization(DP)를 사용한 피보나치 수열 처리 함수
+    :param n: 
+    :return: 
+    """
+    if n <= 1:
+        return memo[n]
+    else:
+        for i in range(2, n+1):
+            memo.append(memo[i-1]+ memo[i-2])
+        return memo[n]
 
-
-def fibo_iter(n):
+def fibo_recu(n):
     r = list()
     p1, p2 = 1, 1
     for _ in range(n-2):
@@ -18,5 +25,5 @@ def fibo_iter(n):
 
 print('피보나치 수 --> 0 1 ')
 for i in range(2, 10) :
-    #print(fibo_iter(i))   # 빠르게 가능
+    # print(fibo_iter(i))   # 빠르게 가능
     print(fibo_recu(i))    # 가면 갈수록 느리다
